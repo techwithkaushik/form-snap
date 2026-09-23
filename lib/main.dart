20345d1095804c402cd5d4e695d6cdd371cf1e00
अरविन्द कौशिक <61283438+ask9027@users.noreply.github.com>
2026-09-23 20:36:58 +0530

    Fix PlatformException import for Dart analyzer

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
 import 'services/image_service.dart';
 import 'services/permission_service.dart';
