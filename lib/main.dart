60c73818371f135fb50eddd09a3f91522b3411f3
अरविन्द कौशिक <61283438+ask9027@users.noreply.github.com>
2026-09-23 20:52:40 +0530

    Fix PermissionStatus extension imports

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/services.dart';
 
 import 'services/image_service.dart';
